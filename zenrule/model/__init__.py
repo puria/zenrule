b907696caca9734909bd294dfa51f0679c8705ac
Puria Nafisi Azizi <puria@dyne.org>
2018-09-14 18:02:35 +0200

    :tada: Initial commit

---
+++ zenrule/model/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""The application's model objects"""
+
+import ming.odm
+from .session import mainsession, DBSession
+
+
+def init_model(engine):
+    """Call me before using any of the tables or classes in the model."""
+    mainsession.bind = engine
+    ming.odm.Mapper.compile_all()
+
+    for mapper in ming.odm.Mapper.all_mappers():
+        mainsession.ensure_indexes(mapper.collection)
+    return DBSession
+
+# Import your model modules here.
+from zenrule.model.auth import User, Group, Permission
+
+__all__ = ('User', 'Group', 'Permission')
